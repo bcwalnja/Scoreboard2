678c6bdcc0c1828c16fb2338485f102f7c2a7b21
Nate Waldron <nate@landistechnologies.com>
2024-09-30 15:25:33 -0400

    added home and away score and document selectors

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 console.log("Welcome to the scoreboard script!");
 
 // gather up the elements we need
+// home-score and away-score
+const homeScore = document.querySelector(".home-score");
+const awayScore = document.querySelector(".away-score");
+
+const homeIncrement = document.querySelector(".home-increment");
+const homeDecrement = document.querySelector(".home-decrement");
+
+const awayIncrement = document.querySelector(".away-increment");
+const awayDecrement = document.querySelector(".away-decrement");
+
+let homeScoreValue = 0;
+let awayScoreValue = 0;
